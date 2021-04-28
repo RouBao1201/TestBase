66ee75e64b9629259d78321c7bd95bd3ed5f3dff
624142800@qq.com <624142800@qq.com>
2021-04-28 21:11:39 +0800

    first commit

---
+++ SpringMVC/src/main/java/com/syb/SpringMvcTestApplication.java
@@ -0,0 +1,17 @@
+package com.syb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: Abin
+ * @Description:
+ * @Date: Created in 21:04 2021/4/28
+ * @Modified By:
+ */
+@SpringBootApplication
+public class SpringMvcTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcTestApplication.class, args);
+    }
+}
---
+++ SpringMVC/src/main/java/com/syb/controller/ValidController.java
@@ -0,0 +1,14 @@
+package com.syb.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Auther: Abin
+ * @Description:
+ * @Date: Created in 21:06 2021/4/28
+ * @Modified By:
+ */
+@Controller
+public class ValidController {
+
+}
